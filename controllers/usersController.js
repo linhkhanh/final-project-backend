ba4fffe4d85632648882bcad8ce6221dc9a4cde9
linhle <linhle@Hieus-MacBook-Air.local>
2020-08-19 16:44:39 +0800

    add authentication

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,64 +1,77 @@
-const  models  = require('../models');
-const { getIdParam, hashPassword } = require('./helper');
+const models = require('../models');
 const httpResponseFormatter = require('../formatters/httpResponse');
+const { getIdParam, hashPassword } = require('./helper');
 
-async function getAll (req, res) {
-    const users = await models.users.findAll();
-    httpResponseFormatter.formatOkResponse(res, users);
-}
+async function getAll(req, res) {
+	const users = await models.users.findAll();
+	httpResponseFormatter.formatOkResponse(res, users);
+};
 
-async function getById (req, res) {
-    const id = getIdParam(req);
-    const user = await models.users.findByPk(id);
-    if (user) {
-        httpResponseFormatter.formatOkResponse(res, user);
-    } else {
-        httpResponseFormatter.formatOkResponse(res, { message: 'This user doesn\'t exist.' });
-    }
-}
+async function getById(req, res) {
+	const id = getIdParam(req);
+	const user = await models.users.findByPk(id);
+	if (user) {
+		httpResponseFormatter.formatOkResponse(res, user);
+	} else {
+		httpResponseFormatter.formatOkResponse(res, { message: "This user doen't exist." });
+	}
+};
 
-async function create (req, res) {
-    if (req.body.id) {
-        httpResponseFormatter.formatOkResponse(res, { message: 'ID should not be provided, since it is determined automatically by the database.' });
-    } else {
-        console.log(req.body);
-        req.body.password = hashPassword(req.body.password);
-        await models.users.create(req.body);
-        httpResponseFormatter.formatOkResponse(res, { message: 'A new user is created.' });
-    }
-}
+async function getByEmail(req, res) {
+	const user = await models.users.findOne({ where: { email: req.body.email } });
+	if (user) {
+		httpResponseFormatter.formatOkResponse(res, user);
+	} else {
+		httpResponseFormatter.formatOkResponse(res, { message: "This user doen't exist." });
+	}
+};
 
-async function update (req, res) {
-    const id = getIdParam(req);
-    console.log(id);
+async function create(req, res) {
+	if (req.body.id) {
+		httpResponseFormatter.formatOkResponse(res, { message: "ID should not be provided, since it is determined automatically by the database." });
+	} else {
+		try {
+			req.body.password = hashPassword(req.body.password);
+			await models.users.create(req.body);
+			httpResponseFormatter.formatOkResponse(res, { message: "A new user is created." });
+		} catch (err) {
+			httpResponseFormatter.formatOkResponse(res, { err: "This email is used already. Please choose another one." });
+		}
+		
+	}
+};
 
-    // We only accept an UPDATE request if the `:id` param matches the body `id`
-    if (id) {
-        await models.users.update(req.body, {
-            where: {
-                id: id
-            }
-        });
-        httpResponseFormatter.formatOkResponse(res, { message: 'Update successfully.' });
-    } else {
-        httpResponseFormatter.formatOkResponse(res, { message: `param ID (${id}) does not match body ID (${req.body.id}).` });
-    }
-}
+async function update(req, res) {
+	const id = getIdParam(req);
 
-async function remove (req, res) {
-    const id = getIdParam(req);
-    await models.users.destroy({
-        where: {
-            id: id
-        }
-    });
-    httpResponseFormatter.formatOkResponse(res, { message: 'Delete successfully.' });
-}
+	// We only accept an UPDATE request if the `:id` param matches the body `id`
+	if (req.body.id === id) {
+		await models.users.update(req.body, {
+			where: {
+				id: id
+			}
+		});
+		httpResponseFormatter.formatOkResponse(res, { message: "Update successfully." });
+	} else {
+		httpResponseFormatter.formatOkResponse(res, { message: `param ID (${id}) does not match body ID (${req.body.id}).` });
+	}
+};
+
+async function remove(req, res) {
+	const id = getIdParam(req);
+	await models.users.destroy({
+		where: {
+			id: id
+		}
+	});
+	httpResponseFormatter.formatOkResponse(res, { message: "Delete successfully." });
+};
 
 module.exports = {
-    getAll,
-    getById,
-    create,
-    update,
-    remove
+	getAll,
+	getById,
+	getByEmail,
+	create,
+	update,
+	remove
 };
--- router/index.js
+++ router/index.js
@@ -1,10 +1,28 @@
 const controllers = {
-    users: require('../controllers/usersController')
+    users: require('../controllers/usersController'),
+    session: require('../controllers/session')
 };
 
 module.exports = app => {
+
+    // check Authentication
+    app.get('/check_authentication', controllers.session.checkAuthentication);
+
+    //get all users
     app.get('/users/all', controllers.users.getAll);
+
+    // logout
+    app.get('/users/logout', controllers.session.logOut);
+
+    //get user by Id
     app.get('/users/:id', controllers.users.getById);
+
+    // create new user
     app.post('/users/new', controllers.users.create);
+
+    // login
+    app.post('/users/login', controllers.session.loginSubmit);
+
+    //update user by id
     app.put('/users/:id', controllers.users.update);
 };
