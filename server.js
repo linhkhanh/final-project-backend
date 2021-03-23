a73f275bd6649328b6f6cc7e7b3f0356006935d9
siewla <siewlapang@gmail.com>
2021-03-23 21:51:25 +0800

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
@@ -53,10 +58,6 @@ if (process.env.NODE_ENV !== 'production') {
     }),
   );
 }
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 app.use(
   cors({
