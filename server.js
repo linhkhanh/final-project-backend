c765f6f58eaa4178304ca4f8d82b35d14c1c6ab3
siewla <siewlapang@gmail.com>
2021-03-23 22:43:33 +0800

    remove cors

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
